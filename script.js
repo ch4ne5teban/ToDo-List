5c7c67e6c8c44da0d38565f8fe54a06fd5a35f02
ch4ne5teban <ca1505pr@outlook.com>
2023-05-08 20:34:32 -0600

    js: add global constants

---
+++ script.js
@@ -0,0 +1,8 @@
+/*-- GLOBAL CONSTANTS --*/
+const taskInput = document.getElementById("task");
+const addTaskBtn = document.getElementById("add-task");
+const resetTasksListBtn = document.getElementById("delete-all-tasks");
+const tasksList = document.getElementById("tasks-list");
+const modalAlertMsg = document.getElementById("modal-alert-msg")
+const closeAlertBtn = document.getElementById("close-alert-btn");
+
