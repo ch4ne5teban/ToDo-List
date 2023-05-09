2a457769a2130cf8eae6b4663b47425a6940b2b1
ch4ne5teban <ca1505pr@outlook.com>
2023-05-08 20:38:26 -0600

    js: feat: 1. add keydown event listener for enter and right arrow keys; 2. add addTaskBtn click event listener

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const tasksList = document.getElementById("tasks-list");
 const modalAlertMsg = document.getElementById("modal-alert-msg")
 const closeAlertBtn = document.getElementById("close-alert-btn");
 
+/*-- ADD TASK SCRIPTS --*/
+taskInput.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter' || event.key === 'ArrowRight') {
+      addTask();
+  }
+});
+
+addTaskBtn.addEventListener('click', addTask);
+
